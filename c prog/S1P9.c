d6f5786fe359536b14c361f5bdb0f76b9e22804d
Akriti Badyal <badyalakriti26@gmail.com>
2025-08-22 23:00:06 +0530

    Add files via upload

---
+++ c prog/S1P9.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ int main(){
+    char *arr[5];
+    arr[0]= "akriti";
+    arr[1]= "riya";
+    arr[2] = "priya";
+    arr[3] = "shine";
+    arr[4] = "rose";
+
+    for(int i=0; i<5; i++){
+        printf("%s\n", arr[i]);
+        
+    }
+
+ }
